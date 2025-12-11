07a11fcc18b89cd18db4f606c19406032adf44f5
Subo117 <subodhikhandare@gmail.com>
2025-12-11 22:01:29 +0530

    Turning Animation DONE

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,16 +7,30 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] GameObject playerModel;
     [SerializeField] float moveSpeed = 100f;
 
+    Animator animator;
     float laneDistance = 15f;
     int currentLane = 0;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.A) && currentLane > -1)
+        {
             currentLane--;
+            animator.Play("Turn Left", 0, 0f);
+        }
 
         if (Input.GetKeyDown(KeyCode.D) && currentLane < 1)
+        {
             currentLane++;
+            animator.Play("Turn Right", 0, 0f);
+        }
+        Debug.Log(animator == null ? "Animator NOT found" : "Animator FOUND");
+
 
         Vector3 targetPos = new Vector3(currentLane * laneDistance, playerModel.transform.position.y, playerModel.transform.position.z);
 
