5b694d425b3751ec2a3faeae7d88210a94c16b7d
Subo117 <subodhikhandare@gmail.com>
2026-01-07 17:48:12 +0530

    Missile Particle

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -4,10 +4,13 @@ public class Missile : MonoBehaviour
 {
     [SerializeField] float missileSpeed = 10f;
     [SerializeField] float rotateSpeed = 10f;
+    [SerializeField] ParticleSystem blastVFX;
+
+    bool isBlast = false;
 
     private void Update()
     {
-        transform.position += Vector3.forward * missileSpeed;
+        transform.position += Vector3.forward * missileSpeed * Time.deltaTime;
         transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
 
         if (transform.position.z > 800) Destroy(this.gameObject);
@@ -18,8 +21,15 @@ public class Missile : MonoBehaviour
         if (collision.gameObject.CompareTag("Car"))
 
         {
+            ParticleSystem vfx = Instantiate(blastVFX, transform.position, Quaternion.identity);
+
+            vfx.Play();
+
+            float totalTime = vfx.main.duration + vfx.main.startLifetime.constantMax;
+
+            Destroy(vfx.gameObject, totalTime);
             Destroy(collision.gameObject);
-            Destroy(this.gameObject);
+            Destroy(gameObject);
         }
     }
 }
