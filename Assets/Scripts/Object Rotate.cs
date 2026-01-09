d79d2811c6b0e56afb1970e3bd6ac5b61b18712b
Subo117 <subodhikhandare@gmail.com>
2026-01-09 09:46:01 +0530

    Wheel rotate

---
+++ Assets/Scripts/Object Rotate.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ObjectRotate : MonoBehaviour
+{
+    [SerializeField] float xRotate = 0f;
+    [SerializeField] float yRotate = 0f;
+    [SerializeField] float zRotate = 0f;
+    [SerializeField] float rotateSpeed = 100f;
+
+    void Update()
+    {
+        transform.Rotate(xRotate * Time.deltaTime, yRotate * Time.deltaTime, zRotate * Time.deltaTime);
+    }
+}
